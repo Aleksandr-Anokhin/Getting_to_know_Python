98b6f70ab033f94850cdfb265647a56b13f2b197
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2022-11-27 23:04:40 +0300

    Solved all tasks

--- Lesson_2/Homework_2.py
+++ Lesson_2/Homework_2.py
@@ -22,4 +22,50 @@ for i in range(N):
     i = i + 1
     f = i * f
     
-    print(f, end = " ")
+print(f, end = " ")
+
+
+# 3. Задайте список из n чисел последовательности (1 + 1 / n)**n и выведите на экран их сумму.
+
+n = int(input("Введите число n: "))
+s = 0
+
+for i in range(n):
+    s += (1 + 1 / n)**n
+    
+print("Сумма чисел последовательности равна: ", s)
+
+# 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных позициях. Позиции вводятся с клавиатуры.
+from random import randint
+
+A = int(input("Введите число N: "))
+numbers = []
+for i in range(A):
+    numbers.append(randint(- A, A))
+print(numbers)
+
+B = int(input("Введите номер первой позиции: "))
+C = int(input("Введите номер второй позиции: "))
+for i in range(len(numbers)):
+    mult = numbers[B -1]*numbers[C - 1]
+print(mult)    
+
+# 5. Реализуйте алгоритм перемешивания списка.
+
+from random import Random, randint
+
+M = int(input("Введите количество элементов массива: "))
+numbers = []
+for i in range(M):
+    numbers.append(randint(- M, M))
+print(numbers)
+
+def mix(numbers):
+    for i in range(len(numbers)):
+        index = randint(0, len(numbers) - 1)
+        temp = numbers[i]
+        numbers[i] = numbers[index]
+        numbers[index] = temp
+    return numbers
+print(mix(numbers))
