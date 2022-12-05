6d00f11064df62bec35a630c86c34f77e5175d3d
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2022-12-05 18:00:56 +0300

    сделал 4 задачи и создал файл для записи

--- Lesson_4/Homework_4.py
+++ Lesson_4/Homework_4.py
@@ -36,12 +36,38 @@ if N > 1:
     multipliers.append(N)
 print(multipliers)
 
+# 3. Задайте последовательность чисел. Напишите программу, 
+# которая выведет список неповторяющихся элементов исходной последовательности.
 
-list_num = [1, 1, 2, 2, 3, 3, 4, 1, 5, 7, 8, 8, 7, 9, 9]
+list_num = [1, 1, 2, 2, 3, 3, 4, 5, 7, 8, 8, 9]
 not_repeat = []
 
 for i in list_num:
-    if i not in not_repeat:
+    if list_num.count(i) == 1:
         not_repeat.append(i)
 print(not_repeat)
 '''
+
+# 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов 
+# (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+import random
+
+k = int(input("Введите число k: "))
+a = int(random.randint(0,100))
+b = int(random.randint(0,100))
+
+if a != 0:
+    polynom_1 = (str(a) + "x^" + str(k) + " + ")
+else:
+    polynom_1 = (str())
+
+if b != 0:
+    polynom_2 = (str(b) + " = 0")
+else:
+    polynom_2 = (str())
+print(f'{polynom_1}{polynom_2}')
+
+with open('degree_k.txt', 'w', encoding='utf8') as file:
+    file.write(f'{polynom_1}{polynom_2}')
+
