865f3749f6d2fe30ba3e85709bd6b96f1f8b788c
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2022-12-13 22:35:32 +0300

    меню работает, запись и чтение файла работает

--- Phonebook/main.py
+++ Phonebook/main.py
@@ -8,39 +8,40 @@ def greeting():
     print('<<ТЕЛЕФОННЫЙ СПРАВОЧНИК>>')
 
 def menu():
-    print('\nМЕНЮ')
-    print('1. Добавить новую запись')
-    print('2. Вывод записей на экран')
-    print('3. Импорт')
-    print('4. Экспорт')
-    print('5. Поиск')
-    print('6. Выход\n') 
-    n = input('Выберите пункт меню: ')
+    while True:
+        print('\nМЕНЮ')
+        print('1. Добавить новую запись')
+        print('2. Вывод записей на экран')
+        print('3. Импорт')
+        print('4. Экспорт')
+        print('5. Поиск')
+        print('6. Выход\n') 
+        n = input('Выберите пункт меню: ')
 
-    if n == '1':
-        new_contact()
-        enter = input('Нажмите Enter для завершения')
-        menu()
-    elif n == '2':
-        myfile = open(filename, 'r+')
-        filecontents = myfile.read()
-        if len(filecontents) == 0:
-            print('Записей нет')
+        if n == '1':
+            new_contact()
+            enter = input('Нажмите Enter для завершения')
+            menu()
+        elif n == '2':
+            myfile = open(filename, 'r+')
+            filecontents = myfile.read()
+            if len(filecontents) == 0:
+                print('Записей нет')
+            else:
+                print(filecontents)
+            myfile.close
+            enter = input('Нажмите Enter для завершения')
+            menu()
+        elif n == '5':
+            searchcontact()
+            enter = input('Нажмите Enter для завершения')
+            menu()
+        elif n == '6':
+            print('Спасибо за просмотр')
         else:
-            print(filecontents)
-        myfile.close
-        enter = input('Нажмите Enter для завершения')
-        menu()
-    elif n == '5':
-        searchcontact()
-        enter = input('Нажмите Enter для завершения')
-        menu()
-    elif n == '6':
-        print('Спасибо за просмотр')
-    else:
-        print('Пожалуста, введите номер пункта меню')
-        enter = input('Нажмите Enter для завершения')
-        menu()
+            print('Пожалуста, введите номер пункта меню')
+            enter = input('Нажмите Enter для завершения')
+            menu()
 
 def input_firstname():
     first = input('Имя: ')
@@ -67,7 +68,7 @@ def new_contact():
     date_of_birth = input('Дата рождения: ')
     phone_number = input('Номер телефона: ')
     mail = input('Эл. почта: ')
-    contact_details = ('[' + first_name + ' ' + last_name + ' ' + patronymic + '; ' + date_of_birth + '; ' + phone_number + '; ' + mail)
+    contact_details = ('[' + first_name + ' ' + last_name + ' ' + patronymic + '; ' + date_of_birth + '; ' + phone_number + '; ' + mail + ']')
     myfile = open(filename, 'a')
     myfile.write(contact_details)
     print('Данные успешно сохранены')
